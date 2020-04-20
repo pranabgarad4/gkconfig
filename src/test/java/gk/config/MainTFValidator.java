8febffcd261dbca53de89b80037759593bb37ffb
pggarad <pranab.g.garad@oracle.com>
2020-04-20 13:02:31 +0530

    added more kv

--- src/test/java/gk/config/MainTFValidator.java
+++ src/test/java/gk/config/MainTFValidator.java
@@ -1,31 +1,48 @@
 package gk.config;
 
 
+import com.google.gson.Gson;
+import gk.config.validators.IValidator;
+import gk.config.validators.PatternValidator;
+import gk.config.validators.ThrottleKv;
 import org.junit.Assert;
+import org.junit.Test;
 
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
 
 public class MainTFValidator {
-
-    private Map<String, Object> kv = new HashMap<>();
+    private Map<String, IValidator> kv = new HashMap<>();
 
     public MainTFValidator() {
-        kv.putIfAbsent("service/app/k1", Pattern.compile("([5-9]|10)"));
-        kv.putIfAbsent("service/app/k2", Pattern.compile("(2[5-9]|30)"));
-        kv.putIfAbsent("service/app/k3", Pattern.compile("(3[5-9]|40)"));
+        kv.putIfAbsent("service/app/k1", new PatternValidator(Pattern.compile("([5-9]|10)")));
+        kv.putIfAbsent("service/app/k2", new PatternValidator(Pattern.compile("(2[5-9]|30)")));
+        kv.putIfAbsent("service/app/k3", new PatternValidator(Pattern.compile("(3[5-9]|40)")));
+        kv.putIfAbsent("gk/throttling", new ThrottleKv());
     }
 
-    public void validate(List<Key> keys) {
-        System.out.println(" validating ... ");
-        for (Key k : keys) {
-            Assert.assertTrue("Invalid key", kv.containsKey(k.getPath()));
-            System.out.println(" key validated");
-            String value = k.getValue();
-            Pattern pattern = (Pattern) kv.get(k.getPath());
-            Assert.assertTrue("Invalid value should be like " + pattern.pattern(), pattern.matcher(value).matches());
+    @Test
+    public void testKeyValue() {
+        try {
+            Reader reader = Files.newBufferedReader(Paths.get(this.getClass().getClassLoader().getResource("config/main.tf.json").toURI()));
+            Root root = new Gson().fromJson(reader, Root.class);
+            List<Key> keys = root.getResource().get(0).getConsul_keys().get(0).getApp().get(0).getKey();
+
+            for (Key k : keys) {
+                Assert.assertTrue("Invalid key", kv.containsKey(k.getPath()));
+                System.out.println(" key validated");
+                String value = k.getValue();
+                IValidator validator = kv.get(k.getPath());
+                validator.validate(k);
+            }
+            reader.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
 }
--- src/test/java/gk/config/ThrottlingTfValidator.java
+++ src/test/java/gk/config/ThrottlingTfValidator.java
@@ -1,14 +1,16 @@
 package gk.config;
 
 import com.google.gson.Gson;
+import com.google.gson.internal.LinkedTreeMap;
 import gk.config.validators.IValidator;
 import gk.config.validators.ThrottleKv;
-import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -20,22 +22,46 @@ public class ThrottlingTfValidator {
         kv.putIfAbsent("gk/throttling", new ThrottleKv());
     }
 
-    @Test
+    @Ignore
     public void testKeyValue() {
         try {
             Reader reader = Files.newBufferedReader(Paths.get(this.getClass().getClassLoader().getResource("config/throttling.tf.json").toURI()));
-            Root root = new Gson().fromJson(reader, Root.class);
-            List<Key> keys = root.getResource().get(0).getConsul_keys().get(0).getApp().get(0).getKey();
 
-            for (Key k : keys) {
-                Assert.assertTrue("Invalid key", kv.containsKey(k.getPath()));
-                System.out.println(" key validated");
-                IValidator validator = kv.get(k.getPath());
-                validator.validate(k);
-            }
+//            Map<?, ?> config = new Gson().fromJson(reader, Map.class);
+            LinkedTreeMap treeMap = new Gson().fromJson(reader, LinkedTreeMap.class);
             reader.close();
+            List resources = (ArrayList) treeMap.get("resource");
+            System.out.println(" resources " + treeMap.toString());
+            Resource resource = new Gson().fromJson(resources.toString(), Resource.class);
+            System.out.println(" Resource " + resource);
+
+//            for(Object key: config.keySet()) {
+//                if(key.equals("resource")) {
+//                    LinkedTreeMap treeMap = (LinkedTreeMap)((ArrayList)config.get(key)).get(0);
+//                    System.out.println(" treeMap " + treeMap);
+//
+//
+////                    Resource resource = new Gson().fromJson((Object) config.get(key), Resource.class);
+////                    System.out.println("Resource " + resource);
+//                }
+////                System.out.println(" key " + key);
+//            }
+////            Root root = new Gson().fromJson(reader, Root.class);
+////            List<Key> keys = root.getResource().get(0).getConsul_keys().get(0).getApp().get(0).getKey();
+////
+////            for (Key k : keys) {
+////                Assert.assertTrue("Invalid key", kv.containsKey(k.getPath()));
+////                System.out.println(" key validated");
+////                IValidator validator = kv.get(k.getPath());
+////                validator.validate(k);
+////            }
+//            reader.close();
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        new ThrottlingTfValidator().testKeyValue();
     }
 }
